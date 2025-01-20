204d4fa80685797f28c78df23b665d6474824ad3
jongwoo <izowooi@hotmail.com>
2025-01-21 01:07:21 +0900

    오디오 추출시 로그 파일 남기도록 수정

--- gen_subtitle/python_proj/extract_audio.py
+++ gen_subtitle/python_proj/extract_audio.py
@@ -1,11 +1,23 @@
 import argparse
 import subprocess
 import os
+import time
+import logging
 
+logging.basicConfig(
+    level=logging.DEBUG,  # 로그 레벨 (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    format='%(asctime)s - %(levelname)s - %(message)s',  # 로그 포맷
+    datefmt='%Y-%m-%d %H:%M:%S',  # 날짜/시간 포맷
+    handlers = [
+        logging.FileHandler("extract_audio_processing.log"),  # 파일 출력
+        logging.StreamHandler()  # 콘솔 출력
+    ]
+)
 
 def trim_video(input_path, output_path, start_time_sec=0, end_time_sec=None):
     if os.path.exists(output_path):
         os.remove(output_path)
+        logging.info(f"기존에 존재하던 파일 삭제: {output_path}")
 
     if end_time_sec is None:
         end_time_sec = start_time_sec + 300
@@ -19,14 +31,17 @@ def trim_video(input_path, output_path, start_time_sec=0, end_time_sec=None):
         "-c", "copy",
         output_path
     ]
+    logging.info(f"동영상 자르기: {input_path} -> {output_path}")
+    logging.debug(f"FFmpeg 명령어: {' '.join(command)}")
     subprocess.run(command, check=True)
+    logging.info(f"동영상 자르기 완료: {output_path}")
     return output_path
 
 
 def extract_audio_to_mp3(input_path, output_audio_path):
-    # 2. 잘라낸 동영상 파일에서 오디오만 mp3로 추출
     if os.path.exists(output_audio_path):
         os.remove(output_audio_path)
+        logging.info(f"기존에 존재하던 파일 삭제: {output_audio_path}")
 
     command = [
         "ffmpeg",
@@ -37,7 +52,10 @@ def extract_audio_to_mp3(input_path, output_audio_path):
         "-b:a", "192k",
         output_audio_path
     ]
+    logging.info(f"오디오 추출 시작: {input_path} -> {output_audio_path}")
+    logging.debug(f"FFmpeg 명령어: {' '.join(command)}")
     subprocess.run(command, check=True)
+    logging.info(f"오디오 추출 완료: {output_audio_path}")
     return output_audio_path
 
 
@@ -59,9 +77,9 @@ if __name__ == "__main__":
     # audio_file = 'audio_0_end.mp3'
     # end_time_sec = 0 * 60 #0분
 
-    print(f'Input video: {original_video}, Output audio: {audio_file}')
-    print(f'Start time: {start_time_sec}')
-    print(f'End time: {end_time_sec}')
+    logging.info(f"프로그램 시작: Input video: {original_video}, Output audio: {audio_file}")
+    logging.info(f"Start time: {start_time_sec}, End time: {end_time_sec}")
+    start_time = time.perf_counter()
 
     if end_time_sec > 0:
         trim_video(original_video, trimmed_video, start_time_sec=start_time_sec, end_time_sec=end_time_sec)
@@ -70,4 +88,8 @@ if __name__ == "__main__":
     else:
         extract_audio_to_mp3(original_video, audio_file)
 
-    print(f"추출된 오디오 파일(mp3): {audio_file}")
+    end_time = time.perf_counter()
+    elapsed_time = end_time - start_time
+
+    logging.info(f"추출된 오디오 파일(mp3): {audio_file}")
+    logging.info(f"소요된 시간: {elapsed_time:.2f} seconds")
