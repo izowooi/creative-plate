bc6b96220f3a94e00273652b1851a63e003c40ae
jongwoo <izowooi@hotmail.com>
2025-01-20 23:38:15 +0900

    깃 무시 파일 추가 ( mp4, srt )

--- gen_subtitle/python_proj/main.py
+++ gen_subtitle/python_proj/main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+import os
+from dotenv import load_dotenv
 
-# Press ⌃F5 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+load_dotenv(verbose=True)
 
+DEEPL_AUTH_KEY = os.getenv('DEEPL_AUTH_KEY')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(f'DEEPL_AUTH_KEY: {DEEPL_AUTH_KEY}')
