36e77dd728cd661520d72d3065c0165572743127
jongwoo <izowooi@hotmail.com>
2025-03-23 22:23:44 +0900

    플러터 기본 뼈대 추가 ( NavigationBar )

---
+++ waffle-wisdom/flutter_proj/lib/main.dart
@@ -0,0 +1,69 @@
+// main.dart
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_proj/widget/empty_widget.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+
+final sceneNameProvider = StateProvider<String>((ref) {
+  return "";
+});
+
+final navigationIndexProvider = StateProvider<int>((ref) {
+  return 0;
+});
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // 바인딩 초기화
+  //await Firebase.initializeApp();
+  
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+      .then((_) {
+    runApp(ProviderScope(child: MainApp()));
+  });
+}
+
+class MainApp extends ConsumerWidget {
+
+  MainApp({super.key});
+
+  final List<String> sceneNames = ['scene_1', 'scene_2', 'scene_3', 'scene_4'];
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentPageIndex = ref.watch(navigationIndexProvider);
+
+    return MaterialApp(
+      home: Builder(builder: (context){
+        return Scaffold(
+        bottomNavigationBar: NavigationBar(
+        destinations: [
+          NavigationDestination(icon: const Icon(Icons.home), label: 'scene_1'),
+          NavigationDestination(icon: const Icon(Icons.favorite), label: 'scene_2'),
+          NavigationDestination(icon: const Icon(Icons.trending_up), label: 'scene_3'),
+          NavigationDestination(icon: const Icon(Icons.settings), label: 'scene_4'),
+        ],
+        selectedIndex: currentPageIndex,
+        onDestinationSelected: (index) {
+            ref.read(sceneNameProvider.notifier).state = sceneNames[index];
+            ref.read(navigationIndexProvider.notifier).state = index;
+          },
+        ),
+        
+        //drawer: LeftDrawerWidget(),
+        body: IndexedStack(
+          index: currentPageIndex,
+          children: [
+            EmptyWidget(),
+            EmptyWidget(),
+            EmptyWidget(),
+            EmptyWidget(),
+          ],
+        ),
+      );
+    })
+    );
+  }
+}
---
+++ waffle-wisdom/flutter_proj/lib/widget/empty_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EmptyWidget extends StatelessWidget {
+  const EmptyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black54,
+      child: const Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
